ad53fa5ca4268efe779d76c635fce8392aa5c1b9
hashiryo <hashimotoryoma@gmail.com>
2024-05-01 14:47:56 +0900

    upd

--- routers/contact.py
+++ routers/contact.py
@@ -1,13 +1,24 @@
 from fastapi import APIRouter
+import schemas.contact as contact_schema
+from datetime import datetime
 router = APIRouter()
 
-@router.get("/contacts")
+@router.get("/contacts", response_model=list[contact_schema.Contact])
 async def get_contact_all():
- pass
+ dummy_data = datetime.now()
+ return [contact_schema.Contact(id=1,
+                                name="山田",
+                                email="test@test.com",
+                                url="http://test.com",
+                                gender=1, 
+                                message="Hello World",
+                                is_enabled=False, 
+                                created_at=dummy_data)]
 
-@router.post("/contacts")
-async def create_contact():
- pass
+@router.post("/contacts", response_model= contact_schema.Contact)
+async def create_contact(body: contact_schema.Contact):
+ return contact_schema.Contact(**body.model_dump())
+ 
 
 @router.get("/contacts/{id}")
 async def get_contact():
